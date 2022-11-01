098cd8819f81ca592c536153ecc78bdf00d272d4
Aleksandr <aleksandrshundryk95@gmail.com>
2022-11-01 09:26:24 +0300

    add default active slide

--- 01-cards/app.js
+++ 01-cards/app.js
@@ -1,18 +1,24 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    if (slide.classList.contains('active')) {
+  slides[activeSlide].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      if (slide.classList.contains('active')) {
+        slide.classList.remove('active');
+      } else {
+        closeCards();
+        slide.classList.add('active');
+      }
+    });
+  }
+
+  function closeCards() {
+    slides.forEach((slide) => {
       slide.classList.remove('active');
-    } else {
-      closeCards();
-      slide.classList.add('active');
-    }
-  });
+    });
+  }
 }
 
-function closeCards() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  });
-}
+slidesPlugin();
